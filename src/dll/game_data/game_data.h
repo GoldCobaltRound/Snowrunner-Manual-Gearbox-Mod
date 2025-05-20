6adab3f4f2b2db11842b7c6fcca9057e811559e3
GoldCobaltRound <2049742540@qq.com>
2025-05-20 22:47:37 +0800

    Update for RoadCraft promo patch

--- src/dll/game_data/game_data.h
+++ src/dll/game_data/game_data.h
@@ -18,18 +18,18 @@ inline const float PowerCoefLowGear = .45f;
 inline const float PowerCoefLowPlusGear = 1.f;
 inline const float PowerCoefLowMinusGear = .2f;
 
-SMGM_DECLARE_PTR(0x2A35BC8, combine_TRUCK_CONTROL *, TruckControlPtr);
+SMGM_DECLARE_PTR(0x2A34D68, combine_TRUCK_CONTROL *, TruckControlPtr);
 } // namespace GameRelatedData
 
 // SMGM_GAME_FUNCTION(0xD5D0B0, void, SwitchAWD, Vehicle *, bool);
-SMGM_GAME_FUNCTION(0xD558E0, bool, ShiftGear, Vehicle*, std::int32_t);
-SMGM_GAME_FUNCTION(0xD55640, std::int32_t, GetMaxGear, const Vehicle*);
-SMGM_GAME_FUNCTION(0xD55690, void, ShiftToAutoGear, Vehicle*);
-SMGM_GAME_FUNCTION(0xB5CEA0, bool, ShiftToReverse, Vehicle*);
-SMGM_GAME_FUNCTION(0xB5CBE0, bool, ShiftToNeutral, Vehicle*);
-SMGM_GAME_FUNCTION(0xB5CAC0, bool, ShiftToHigh, Vehicle*);
-SMGM_GAME_FUNCTION(0xD59390, bool, DisableAutoAndShift, Vehicle*,
-                   std::int32_t);
-SMGM_GAME_FUNCTION(0xD54A80, void, SetPowerCoef, Vehicle*, float);
-SMGM_GAME_FUNCTION(0xACC460, void, SetCurrentVehicle, combine_TRUCK_CONTROL*,
-                   Vehicle *);
+SMGM_GAME_FUNCTION(0xD55C40, bool, ShiftGear, Vehicle *, std::int32_t);
+SMGM_GAME_FUNCTION(0xD559A0, std::int32_t, GetMaxGear, const Vehicle *);
+SMGM_GAME_FUNCTION(0xD559F0, void, ShiftToAutoGear, Vehicle *);
+SMGM_GAME_FUNCTION(0xB5CDA0, bool, ShiftToReverse, Vehicle *);
+SMGM_GAME_FUNCTION(0xB5CAE0, bool, ShiftToNeutral, Vehicle *);
+SMGM_GAME_FUNCTION(0xB5C9C0, bool, ShiftToHigh, Vehicle *);
+SMGM_GAME_FUNCTION(0xD596F0, bool, DisableAutoAndShift, Vehicle *,
+    std::int32_t);
+SMGM_GAME_FUNCTION(0xD54DE0, void, SetPowerCoef, Vehicle *, float);
+SMGM_GAME_FUNCTION(0xACC360, void, SetCurrentVehicle, combine_TRUCK_CONTROL *,
+    Vehicle *);
